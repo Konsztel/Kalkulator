515b4d2f6ff1b9079648d2c304e4b0987c05c2b7
Paweł <pawcio574@interia.pl>
2023-12-15 21:01:15 +0100

    c

---
+++ Kalk.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Kalk {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę");
+        double liczba1 = scan.nextInt();
+        scan.nextLine(); //potrzebne, jeśli wczytujemy napis po wczytaniu liczby
+
+        System.out.println("Podaj operator + - * /");
+        new Scanner(System.in);
+        String operator = scan.nextLine();
+
+        System.out.println("Podaj drugą liczbę");
+        new Scanner(System.in);
+        double liczba2 = scan.nextInt();
+        scan.close();
+
+        if (operator.equals("+")) {                    //Wykonaj dodawanie
+            double suma = liczba1 + liczba2;
+            System.out.println("Wynik to " + suma);
+
+        }   else if (operator.equals("-")){             //Wykonaj odejmowanie;
+            double suma = liczba1 - liczba2;
+            System.out.println("Wynik to " + suma);
+
+        }   else if (operator.equals("*")) {            //Wykonaj mnożenie;
+            double suma = liczba1 * liczba2;
+            System.out.println("Wynik to " + suma);
+
+        }   else if (operator.equals("/")){             //Wykonaj dzielenie
+            double suma = liczba1 / liczba2;
+            System.out.println("Wynik to " + suma);
+
+        }   else {System.out.println("Podałeś nie dozwolony operator. Podaj poprawy operator: + - * /");
+
+        }
+
+    }
+}
